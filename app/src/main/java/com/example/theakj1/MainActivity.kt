2599257de29aa109701339472daa2db960bc9be7
The Abhishek Jaiswal <abhivishaljaisdwal420@gmail.com>
2023-04-21 22:40:40 +0530

    java main updated

--- app/src/main/java/com/example/theakj1/MainActivity.kt
+++ app/src/main/java/com/example/theakj1/MainActivity.kt
@@ -9,4 +9,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    override fun setFinishOnTouchOutside(finish: Boolean) {
+        super.setFinishOnTouchOutside(finish)
+    }
+
 }
